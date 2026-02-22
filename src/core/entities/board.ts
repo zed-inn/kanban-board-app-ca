92312aa9380baafeaaa2bab2caf6208d2346c471
Zed <skhull.coding@gmail.com>
2026-02-23 00:43:49 +0530

    feat: methods for board

--- src/core/entities/board.ts
+++ src/core/entities/board.ts
@@ -23,4 +23,23 @@ export class Board {
 
     this.ownerId = params.ownerId;
   }
+
+  updateName = (name: string) => {
+    if (typeof name !== "string") throw new Error("Invalid board name.");
+    name = name.trim();
+    if (name.length < 1) throw new Error("Board name cannot be empty.");
+
+    this.name = name;
+  };
+
+  transferOwnershipTo = (ownerId: string) => {
+    if (typeof ownerId !== "string") throw new Error("Invalid owner id.");
+    ownerId = ownerId.trim();
+    if (ownerId.length < 1) throw new Error("Owner id cannot be empty.");
+
+    if (ownerId === this.ownerId)
+      throw new Error("Ownership transfer to self is not allowed.");
+
+    this.ownerId = ownerId;
+  };
 }
