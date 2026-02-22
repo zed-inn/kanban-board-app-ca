c784ae12ac8d488a264efe1cd61d0dbc24640426
Zed <skhull.coding@gmail.com>
2026-02-23 01:17:19 +0530

    feat: column and card attrbs and methods

--- src/core/entities/card.ts
+++ src/core/entities/card.ts
@@ -1,21 +1,78 @@
 export class Card {
   public id: string;
   public title: string;
-  public content: string;
+  public content: string | null;
   public position: number;
-  protected columnId: string;
+  public columnId: string;
 
   constructor(params: {
     id: string;
     title: string;
-    content: string;
+    content: string | null;
     position: number;
     columnId: string;
   }) {
+    if (typeof params.id !== "string") throw new Error("Invalid card id.");
+    params.id = params.id.trim();
+    if (params.id.length < 1) throw new Error("Card Id cannot be empty.");
+
     this.id = params.id;
+
+    if (typeof params.title !== "string")
+      throw new Error("Invalid card title.");
+    params.title = params.title.trim();
+    if (params.title.length < 1) throw new Error("Card title cannot be empty.");
+
     this.title = params.title;
-    this.content = params.content;
+
+    if (!(typeof params.content === "string" || params.content === null))
+      throw new Error("Invalid card content.");
+
+    this.content = params.content?.trim() ?? null;
+
+    if (typeof params.position !== "number")
+      throw new Error("Invalid card position.");
+
     this.position = params.position;
+
+    if (typeof params.columnId !== "string")
+      throw new Error("Invalid column id.");
+    params.columnId = params.columnId.trim();
+    if (params.columnId.length < 1)
+      throw new Error("Column Id cannot be empty.");
+
     this.columnId = params.columnId;
   }
+
+  updateBody = (params: { title?: string; content?: string | null }) => {
+    if (params.title !== undefined) {
+      if (typeof params.title !== "string")
+        throw new Error("Invalid card title.");
+      params.title = params.title.trim();
+      if (params.title.length < 1)
+        throw new Error("Card title cannot be empty.");
+
+      this.title = params.title;
+    }
+
+    if (params.content !== undefined) {
+      if (!(typeof params.content === "string" || params.content === null))
+        throw new Error("Invalid card content.");
+
+      this.content = params.content?.trim() ?? null;
+    }
+  };
+
+  updatePosition = (position: number) => {
+    if (typeof position !== "number") throw new Error("Invalid card position.");
+    this.position = position;
+  };
+
+  updateColumn = (columnId: string) => {
+    if (typeof columnId !== "string") throw new Error("Invalid column id.");
+    columnId = columnId.trim();
+    if (columnId.length < 1) throw new Error("Column Id cannot be empty.");
+
+    this.columnId = columnId;
+  };
 }
--- src/core/entities/column.ts
+++ src/core/entities/column.ts
@@ -10,9 +10,44 @@ export class Column {
     position: number;
     boardId: string;
   }) {
+    if (typeof params.id !== "string") throw new Error("Invalid column id.");
+    params.id = params.id.trim();
+    if (params.id.length < 1) throw new Error("Column Id cannot be empty.");
+
     this.id = params.id;
+
+    if (typeof params.name !== "string")
+      throw new Error("Invalid column name.");
+    params.name = params.name.trim();
+    if (params.name.length < 1) throw new Error("Column name cannot be empty.");
+
     this.name = params.name;
+
+    if (typeof params.position !== "number")
+      throw new Error("Invalid column position.");
+
     this.position = params.position;
+
+    if (typeof params.boardId !== "string")
+      throw new Error("Invalid board id.");
+    params.boardId = params.boardId.trim();
+    if (params.boardId.length < 1) throw new Error("Board Id cannot be empty.");
+
     this.boardId = params.boardId;
   }
+
+  updateName = (name: string) => {
+    if (typeof name !== "string") throw new Error("Invalid column name.");
+    name = name.trim();
+    if (name.length < 1) throw new Error("Column name cannot be empty.");
+
+    this.name;
+  };
+
+  updatePosition = (position: number) => {
+    if (typeof position !== "number")
+      throw new Error("Invalid column position.");
+
+    this.position = position;
+  };
 }
