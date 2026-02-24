398e34591f9f147e9b59bd205c70a25078d5453c
Zed <skhull.coding@gmail.com>
2026-02-24 10:40:28 +0530

    fix: creating id rather than supplying it

--- src/core/use-cases/board/create-board.usecase.ts
+++ src/core/use-cases/board/create-board.usecase.ts
@@ -3,16 +3,20 @@ import { BoardMembership } from "../../entities/board_membership";
 import type { BoardRepository } from "../../interfaces/board-repository.interface";
 import type { MemberRepository } from "../../interfaces/member-repository.interface";
 import type { UnitOfWork } from "../../interfaces/unit-of-work.interface";
+import type { IdGenerator } from "../../interfaces/utils/id-generator.interface";
 
 export class CreateBoard {
   constructor(
+    private idGen: IdGenerator,
     private boardRepo: BoardRepository,
     private memberRepo: MemberRepository,
     private uow: UnitOfWork,
   ) {}
 
-  execute = async (boardId: string, name: string, userId: string) => {
-    return await this.uow.withTransaction(async () => {
+  execute = async (name: string, userId: string) => {
+    const boardId = this.idGen.generate();
+
+    await this.uow.withTransaction(async () => {
       const board = new Board({ id: boardId, name, ownerId: userId });
       await this.boardRepo.save(board);
 
