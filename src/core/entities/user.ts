4d56fc00ef360f7f702c30a2ead77474b6013b1f
Zed <skhull.coding@gmail.com>
2026-02-26 12:36:23 +0530

    chore: reverted to _id and id controlled access via getters

--- src/core/entities/user.ts
+++ src/core/entities/user.ts
@@ -1,15 +1,19 @@
 export class User {
-  private id: string;
+  private _id: string;
 
   constructor(params: { id: string }) {
     if (typeof params.id !== "string") throw new Error("Invalid user id.");
     params.id = params.id.trim();
     if (params.id.length < 1) throw new Error("User id cannot be empty.");
 
-    this.id = params.id;
+    this._id = params.id;
   }
 
   public get attrbs() {
-    return { id: this.id };
+    return { id: this._id };
+  }
+
+  public get id() {
+    return this._id;
   }
 }
