fac2c833fffbe1f2e10caa6077dbb4e074dc9464
Zed <skhull.coding@gmail.com>
2026-03-01 04:35:33 +0530

    chore: export all interface on same level as index

---
+++ src/interface.ts
@@ -0,0 +1,11 @@
+export type { EventEmitter } from "./core/interfaces/emitter/event-emitter.interface";
+export type { CardPolicy } from "./core/interfaces/policy/card-policy.interface";
+export type { ColumnPolicy } from "./core/interfaces/policy/column-policy.interface";
+export type { MemberPolicy } from "./core/interfaces/policy/member-policy.interface";
+export type { BoardRepository } from "./core/interfaces/repo/board-repository.interface";
+export type { CardRepository } from "./core/interfaces/repo/card-repository.interface";
+export type { ColumnRepository } from "./core/interfaces/repo/column-repository.interface";
+export type { MemberRepository } from "./core/interfaces/repo/member-repository.interface";
+export type { IdGenerator } from "./core/interfaces/utils/id-generator.interface";
+export type { UnitOfWork } from "./core/interfaces/utils/unit-of-work.interface";
+
